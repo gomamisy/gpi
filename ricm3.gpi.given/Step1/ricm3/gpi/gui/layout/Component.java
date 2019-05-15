b0b26c01aa80cd14589e4b379a7166665c370cc8
yoann_gm <yoann.goma@hotmail.fr>
2019-05-15 14:44:45 +0200

    step 1 (début)

--- ricm3.gpi.given/Step1/ricm3/gpi/gui/layout/Component.java
+++ ricm3.gpi.given/Step1/ricm3/gpi/gui/layout/Component.java
@@ -5,201 +5,249 @@ import ricm3.gpi.gui.Graphics;
 import ricm3.gpi.gui.Window;
 
 /**
- * This is a component within a tree of containers and components.
- * Each component has a surface, defined by its bounds, within
- * the surface of its parent. 
+ * This is a component within a tree of containers and components. Each
+ * component has a surface, defined by its bounds, within the surface of its
+ * parent.
  * 
- * A component may be ask to paint itself, at any moment, 
- * by invoking the method: paint(Graphics g) 
+ * A component may be ask to paint itself, at any moment, by invoking the
+ * method: paint(Graphics g)
  * 
- * MouseListener:
- *   When the mouse enters, leaves, is moved, or clicked within 
- *   the surface of a component, the mouse listener of this 
- *   component is told about these events, if there is a mouse
- *   listener associated with this component at the time of the event.
+ * MouseListener: When the mouse enters, leaves, is moved, or clicked within the
+ * surface of a component, the mouse listener of this component is told about
+ * these events, if there is a mouse listener associated with this component at
+ * the time of the event.
  * 
- * KeyListener:
- *   If the mouse is currently within this component and a key
- *   is pressed or released on the keyboard, the key listener of this 
- *   component is told about these events, if there is a key
- *   listener associated with this component at the time of the event.
+ * KeyListener: If the mouse is currently within this component and a key is
+ * pressed or released on the keyboard, the key listener of this component is
+ * told about these events, if there is a key listener associated with this
+ * component at the time of the event.
  *
  * @author Pr. Olivier Gruber (olivier dot gruber at acm dot org)
  */
 
 public class Component {
 
-  protected Container m_parent;
-  protected int m_x, m_y, m_width, m_height; // bounds for this component.
-  ricm3.gpi.gui.KeyListener m_kl; // key listener for this component
-  ricm3.gpi.gui.MouseListener m_ml; // mouse listener for this component
-  protected Color m_bgColor; // background color for this component
-  protected Color m_fgColor; // foreground color for this component
-  
-  protected Component() {
-    throw new Error("Not Yet Implemented");
-  }
+	protected Container m_parent;
+	protected int m_x, m_y, m_width, m_height; // bounds for this component.
+	ricm3.gpi.gui.KeyListener m_kl; // key listener for this component
+	ricm3.gpi.gui.MouseListener m_ml; // mouse listener for this component
+	protected Color m_bgColor; // background color for this component
+	protected Color m_fgColor; // foreground color for this component
 
-  public Component(Container parent) {
-    throw new Error("Not Yet Implemented");
-  }
+	protected Component() {
+		m_parent = null;
+		m_x = 0;
+		m_y = 0;
+		m_width = 0;
+		m_height = 0;
+		m_bgColor = Color.BLACK;
+		m_fgColor = Color.WHITE;
+	}
 
-  public String toString() {
-    return getClass().getSimpleName() + "(" + m_x + "," + m_y + "," + m_width + "," + m_height + "," + ")";
-  }
+	public Component(Container parent) {
+		m_parent = parent;
+		m_parent.m_children.add(this);
+		m_x = 0;
+		m_y = 0;
+		m_width = 0;
+		m_height = 0;
+		m_bgColor = Color.BLACK;
+		m_fgColor = Color.WHITE;
+	}
 
-  public void setForeground(Color c) {
-    m_fgColor = c;
-  }
+	public String toString() {
+		return getClass().getSimpleName() + "(" + m_x + "," + m_y + "," + m_width + "," + m_height + "," + ")";
+	}
 
-  public void setBackground(Color c) {
-    m_bgColor = c;
-  }
+	public void setForeground(Color c) {
+		m_fgColor = c;
+	}
 
-  /**
-   * Set the key listener for this component.
-   * @param kl is the key listener
-   */
-  public void setKeyListener(ricm3.gpi.gui.KeyListener kl) {
-    m_kl = kl;
-  }
+	public void setBackground(Color c) {
+		m_bgColor = c;
+	}
 
-  /**
-   * Set the mouse listener for this component.
-   * All callbacks with coordinates will be in local coordinates.
-   * @param kl is the key listener
-   */
-  public void setMouseListener(ricm3.gpi.gui.MouseListener ml) {
-    m_ml = ml;
-  }
+	/**
+	 * Set the key listener for this component.
+	 * 
+	 * @param kl is the key listener
+	 */
+	public void setKeyListener(ricm3.gpi.gui.KeyListener kl) {
+		m_kl = kl;
+	}
 
-  /**
-   * Convert a global location to a location that is local to this component
-   * @param l
-   */
-  public void toLocal(Location l) {
-    throw new Error("Not Yet Implemented");
-  }
+	/**
+	 * Set the mouse listener for this component. All callbacks with coordinates
+	 * will be in local coordinates.
+	 * 
+	 * @param kl is the key listener
+	 */
+	public void setMouseListener(ricm3.gpi.gui.MouseListener ml) {
+		m_ml = ml;
+	}
 
-  /**
-   * Convert a local location to a location that is global, that is, in the
-   * coordinates of the root of the layout tree.
-   * @param l
-   */
-  public void toGlobal(Location l) {
-    throw new Error("Not Yet Implemented");
-  }
+	/**
+	 * Convert a global location to a location that is local to this component
+	 * 
+	 * @param l
+	 */
+	public void toLocal(Location l) {
+		Component courrent = this;
+		int x = 0;
+		int y = 0;
 
-  public Component parent() {
-    return m_parent;
-  }
+		while (courrent.m_parent != null) {
+			x += courrent.m_x;
+			y += courrent.m_y;
+			courrent = courrent.m_parent;
+		}
 
-  public void remove() {
-    throw new Error("Not Yet Implemented");
-  }
+		l.set(l.x() - x, l.y() - y);
+	}
 
-  public Location location() {
-    return new Location(m_x, m_y);
-  }
+	/**
+	 * Convert a local location to a location that is global, that is, in the
+	 * coordinates of the root of the layout tree.
+	 * 
+	 * @param l
+	 */
+	public void toGlobal(Location l) {
+		Component courrent = this;
+		int x = 0;
+		int y = 0;
 
-  public void location(Location loc) {
-    m_x = loc.x();
-    m_y = loc.y();
-  }
+		while (courrent.m_parent != null) {
+			x += courrent.m_x;
+			y += courrent.m_y;
+			courrent = courrent.m_parent;
+		}
 
-  public int x() {
-    return m_x;
-  }
+		l.set(l.x() + x, l.y() + y);
+	}
 
-  public int y() {
-    return m_y;
-  }
+	public Component parent() {
+		return m_parent;
+	}
 
-  public int width() {
-    return m_width;
-  }
+	public void remove() {
+		if (m_parent == null) {
+			throw new IllegalStateException("Remove is not allowed");
+		}
 
-  public int height() {
-    return m_height;
-  }
+		m_parent.m_children.remove(this);
+		m_parent = null;
+	}
 
-  public void setLocation(int x, int y) {
-    m_x = x;
-    m_y = y;
-  }
+	public Location location() {
+		return new Location(m_x, m_y);
+	}
 
-  public Dimension dimension() {
-    return new Dimension(m_width, m_height);
-  }
+	public void location(Location loc) {
+		m_x = loc.x();
+		m_y = loc.y();
+	}
 
-  public void setDimension(Dimension dim) {
-    m_width = dim.width();
-    m_height = dim.height();
-  }
+	public int x() {
+		return m_x;
+	}
 
-  public void setDimension(int w, int h) {
-    m_width = w;
-    m_height = h;
-  }
+	public int y() {
+		return m_y;
+	}
 
-  public void setBounds(Location loc, Dimension dim) {
-    m_x = loc.x();
-    m_y = loc.y();
-    m_width = dim.width();
-    m_height = dim.height();
-  }
+	public int width() {
+		return m_width;
+	}
 
-  public void setBounds(int x, int y, int w, int h) {
-    m_x = x;
-    m_y = y;
-    m_width = w;
-    m_height = h;
-  }
+	public int height() {
+		return m_height;
+	}
 
-  /**
-   * Tells if the given location is within this component.
-   * The location is given in the parent's coordinates.
-   * @param x
-   * @param y
-   * @return
-   */
-  public boolean inside(int x, int y) {
-    throw new Error("Not Yet Implemented");
-  }
+	public void setLocation(int x, int y) {
+		m_x = x;
+		m_y = y;
+	}
 
-  /**
-   * Tells if the given location is within this component.
-   * The location is given in local coordinates.
-   * @param x
-   * @param y
-   * @return
-   */
-  public Component select(int x, int y) {
-    throw new Error("Not Yet Implemented");
-  }
+	public Dimension dimension() {
+		return new Dimension(m_width, m_height);
+	}
 
-  /**
-   * Paint this component using the given graphics.
-   * The graphics work in local coordinates and
-   * all drawing/fill operations are clipped to 
-   * this component.
-   * @param g
-   */
-  public void paint(Graphics g) {
-    g.setColor(m_bgColor);
-    g.fillRect(0, 0, m_width, m_height);
-  }
+	public void setDimension(Dimension dim) {
+		m_width = dim.width();
+		m_height = dim.height();
+	}
 
-  /**
-   * Ask a repaint of this component.
-   * The result will be that at some later time, 
-   * the paint method of this component will be called.
-   */
-  public void repaint() {
-    Window win = Window.getWindow();
-    Location l = new Location(0, 0);
-    toGlobal(l);
-    win.repaint(l.x(), l.y(), m_width, m_height);
-  }
+	public void setDimension(int w, int h) {
+		m_width = w;
+		m_height = h;
+	}
+
+	public void setBounds(Location loc, Dimension dim) {
+		m_x = loc.x();
+		m_y = loc.y();
+		m_width = dim.width();
+		m_height = dim.height();
+	}
+
+	public void setBounds(int x, int y, int w, int h) {
+		m_x = x;
+		m_y = y;
+		m_width = w;
+		m_height = h;
+	}
+
+	/**
+	 * Tells if the given location is within this component. The location is given
+	 * in the parent's coordinates.
+	 * 
+	 * @param x
+	 * @param y
+	 * @return
+	 */
+	public boolean inside(int x, int y) {
+		if ((x >= m_x && x < m_x + m_width) && (y >= m_y && y < m_y + m_height)) {
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Tells if the given location is within this component. The location is given
+	 * in local coordinates.
+	 * 
+	 * @param x
+	 * @param y
+	 * @return
+	 */
+	public Component select(int x, int y) {
+		if (inside(x, y)) {
+			return this;
+		}
+		return null;
+	}
+
+	/**
+	 * Paint this component using the given graphics. The graphics work in local
+	 * coordinates and all drawing/fill operations are clipped to this component.
+	 * 
+	 * @param g
+	 */
+	public void paint(Graphics g) {
+		g.setColor(m_bgColor);
+		Location loc = new Location(0, 0);
+		toGlobal(loc);
+		g.fillRect(loc.x(), loc.y(), m_width, m_height);
+	}
+
+	/**
+	 * Ask a repaint of this component. The result will be that at some later time,
+	 * the paint method of this component will be called.
+	 */
+	public void repaint() {
+		Window win = Window.getWindow();
+		Location l = new Location(0, 0);
+		toGlobal(l);
+		win.repaint(l.x(), l.y(), m_width, m_height);
+	}
 
 }
--- ricm3.gpi.given/Step1/ricm3/gpi/gui/layout/Container.java
+++ ricm3.gpi.given/Step1/ricm3/gpi/gui/layout/Container.java
@@ -1,63 +1,97 @@
 package ricm3.gpi.gui.layout;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
 import ricm3.gpi.gui.Graphics;
 
 /**
- * This is a container within a tree of containers and components.
- * A container is a component that has children components.
- * Children components are painted on top of their parent container.
+ * This is a container within a tree of containers and components. A container
+ * is a component that has children components. Children components are painted
+ * on top of their parent container.
  * 
  * @author Pr. Olivier Gruber (olivier dot gruber at acm dot org)
  */
 public class Container extends Component {
-  
-  Container() {
-    throw new Error("Not Yet Implemented");
-  }
+	protected List<Component> m_children;
 
-  public Container(Container parent) {
-    super(parent);
-    throw new Error("Not Yet Implemented");
-  }
+	Container() {
+		m_parent = null;
+		m_children = new LinkedList<Component>();
+		m_x = 0;
+		m_y = 0;
+		m_width = 0;
+		m_height = 0;
+	}
 
-  /**
-   * @return the number of components that are 
-   *         children to this container
-   */
-  public int childrenCount() {
-    throw new Error("Not Yet Implemented");
-  }
+	public Container(Container parent) {
+		super(parent);
+		parent.m_children.add(this);
+		m_parent = parent;
+		m_children = new LinkedList<Component>();
+		m_x = 0;
+		m_y = 0;
+		m_width = 0;
+		m_height = 0;
+	}
 
-  /**
-   * @return the component indexed by the given
-   *         index.
-   */
-  public Component childrenAt(int i) {
-    throw new Error("Not Yet Implemented");
-  }
+	/**
+	 * @return the number of components that are children to this container
+	 */
+	public int childrenCount() {
+		if (m_children != null) {
+			return m_children.size();
+		}
+		return 0;
+	}
 
+	/**
+	 * @return the component indexed by the given index.
+	 */
+	public Component childrenAt(int i) {
+		return m_children.get(i);
+	}
 
-  /**
-   * Select the component, on top, at the given location.
-   * The location is given in local coordinates.
-   * Reminder: children are above their parent.
-   * @param x
-   * @param y
-   * @return this selected component 
-   */
-  public Component select(int x, int y) {
-    throw new Error("Not Yet Implemented");
-  }
+	/**
+	 * Select the component, on top, at the given location. The location is given in
+	 * local coordinates. Reminder: children are above their parent.
+	 * 
+	 * @param x
+	 * @param y
+	 * @return this selected component
+	 */
+	public Component select(int x, int y) {
+		if (inside(x, y)) {
+			Component courrentComp;
+			Component resComp = this;
+			Iterator<Component> iter = m_children.listIterator();
+			while (iter.hasNext()) {
+				courrentComp = iter.next();
+				if (courrentComp.inside(x, y)) {
+					resComp = courrentComp.select(x, y);
+				}
+			}
+			return resComp;
+		} else {
+			return null;
+		}
+	}
 
-  /**
-   * Painting a container is a two-step process
-   * in order to paint children components above.
-   *    - First, the container paints itself.
-   *    - Second, the container paints its children
-   */
-  public void paint(Graphics g) {
-    super.paint(g);
-    throw new Error("Not Yet Implemented");
-  }
-  
+	/**
+	 * Painting a container is a two-step process in order to paint children
+	 * components above. - First, the container paints itself. - Second, the
+	 * container paints its children
+	 */
+	public void paint(Graphics g) {
+		super.paint(g);
+		Component courrentComp;
+		Iterator<Component> iter = m_children.listIterator();
+		while (iter.hasNext()) {
+			courrentComp = iter.next();
+			courrentComp.paint(g);
+		}
+
+	}
+
 }
